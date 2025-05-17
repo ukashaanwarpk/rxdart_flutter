d1be75c7b02a6172146f42fd22f823b39479c2dd
ukashaanwarpk <anwarukasha0341@gmail.com>
2025-05-17 19:08:15 +0500

    Add Api class for searching and caching Person and Animal data

---
+++ lib/bloc/api.dart
@@ -0,0 +1,84 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:rxdart_flutter/models/animal.dart';
+import 'package:rxdart_flutter/models/person.dart';
+import 'package:rxdart_flutter/models/thing.dart';
+
+typedef SearchTerm = String;
+
+class Api {
+  List<Person>? _persons;
+  List<Animal>? _animals;
+
+  Api();
+
+  Future<List<Thing>> search(SearchTerm searchTerm) async {
+    final term = searchTerm.trim().toLowerCase();
+
+    // search in the cache
+
+    final cachedResults = _extractThingsUsingSearchTerm(term);
+
+    if (cachedResults != null) {
+      return cachedResults;
+    }
+
+    // so we don't have the values cached, let's call api
+
+    final persons = await _getJson(
+      'http://127.0.0.1:5500/apis/persons.json',
+    ).then((json) => json.map((value) => Person.fromJson(value)));
+
+    _persons = persons.toList();
+
+    final animals = await _getJson(
+      'http://127.0.0.1:5500/apis/animals.json',
+    ).then((json) => json.map((value) => Animal.fromJson(value)));
+
+    _animals = animals.toList();
+
+    return _extractThingsUsingSearchTerm(term) ?? [];
+  }
+
+  List<Thing>? _extractThingsUsingSearchTerm(SearchTerm term) {
+    final cachedAnimals = _animals;
+    final cachedPersons = _persons;
+
+    if (cachedPersons != null && cachedAnimals != null) {
+      List<Thing> result = [];
+
+      // go through animals
+
+      for (var animal in cachedAnimals) {
+        if (animal.name.trimmedContain(term) ||
+            animal.type.name.trimmedContain(term)) {
+          result.add(animal);
+        }
+      }
+
+      // go through persons
+
+      for (var person in cachedPersons) {
+        if (person.name.trimmedContain(term) ||
+            person.age.toString().trimmedContain(term)) {
+          result.add(person);
+        }
+      }
+      return result;
+    } else {
+      return null;
+    }
+  }
+
+  Future<List<dynamic>> _getJson(String url) => HttpClient()
+      .getUrl(Uri.parse(url))
+      .then((req) => req.close())
+      .then((response) => response.transform(utf8.decoder).join())
+      .then((jsonString) => json.decode(jsonString) as List<dynamic>);
+}
+
+extension TrimmedCaseInsensitiveContain on String {
+  bool trimmedContain(String other) =>
+      trim().toLowerCase().contains(other.trim().toLowerCase());
+}
---
+++ lib/bloc/search_result.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:rxdart_flutter/models/thing.dart';
+
+@immutable
+abstract class SearchResult {
+  const SearchResult();
+}
+
+@immutable
+class SearchResultLoading implements SearchResult {
+  const SearchResultLoading();
+}
+
+@immutable
+class SearchResultNoResult implements SearchResult {
+  const SearchResultNoResult();
+}
+
+@immutable
+class SearchResultHasError implements SearchResult {
+  final Object error;
+  const SearchResultHasError(this.error);
+}
+
+@immutable
+class SearchResultWithResult implements SearchResult {
+  final List<Thing> results;
+  const SearchResultWithResult(this.results);
+}
