0c0625e622786d555c4faadee03a07d5d706f622
ukashaanwarpk <anwarukasha0341@gmail.com>
2025-05-22 09:46:01 +0500

    Update main.dart to switch home widget from ConcatStreamExample to MergeStreamExample and correct import statement

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:rxdart_flutter/behaviour_subject_example.dart';
 import 'package:rxdart_flutter/combine_stream_example.dart';
-import 'package:rxdart_flutter/views/concat_stream_example.dart';
+import 'package:rxdart_flutter/concat_stream_example.dart';
+import 'package:rxdart_flutter/merge_stream_example.dart';
 import 'package:rxdart_flutter/views/home_view.dart';
 
 void main() {
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: ConcatStreamExample(),
+      home: MergeStreamExample(),
     );
   }
 }
