8a52875d17125eae765e75b70954833ea5ba9743
ukashaanwarpk <anwarukasha0341@gmail.com>
2025-06-07 12:43:05 +0500

    Add type definitions for contact management callbacks

---
+++ lib/contacts_app/type_defination.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'models/contact_model.dart';
+
+typedef LogoutCallback = VoidCallback;
+typedef GoBackCallback = VoidCallback;
+
+typedef LoginFunction = void Function(String email, String password);
+
+typedef RegisterFunction = void Function(String email, String password);
+
+typedef CreateContactCallback =
+    void Function(String firstName, String lastName, String phoneNumber);
+
+typedef DeleteContactCallback = void Function(Contact contact);
